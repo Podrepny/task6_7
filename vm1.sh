53c4f317e9ec3ffdeddaca8673129f40981c60b6
Alexey <no@mail.here>
2018-04-06 12:39:47 -0700

    Step 1 and 2 - done for VM1

--- vm1.sh
+++ vm1.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+source vm1.config
+if [ "$EXT_IP" == "DHCP" ]; then
+     dhclient $EXTERNAL_IF
+else
+     ifconfig $EXTERNAL_IF $EXT_IP
+     route add default gw `echo $EXT_GW | sed 's/\/.*$//g'`
+fi
 
+# Setup VLAN on INTERNAL_IF
+apt-get -y install vlan
+vconfig add $INTERNAL_IF $VLAN
+ifconfig $INTERNAL_IF 0/0 up 2>/dev/null
+ifconfig $INTERNAL_IF.$VLAN $VLAN_IP up
