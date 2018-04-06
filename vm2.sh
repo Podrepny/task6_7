e20608ebcbb6a0704f9e326bea2d0b32a81e8531
Alexey <you@example.com>
2018-04-06 16:01:29 -0700

    fix internal VM2 connectivity #2

--- vm2.sh
+++ vm2.sh
@@ -2,10 +2,14 @@
 
 source vm2.config
 
+# setup internet routing
+ifconfig $INTERNAL_IF $INTERNAL_IP up
+route add default gw `grep ^INT_IP= vm1.config | sed 's/^INT_IP=\(.*\)\/.*$/\1/g'`
+echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+
 # Setup VLAN on INTERNAL_IF
 apt-get -y install vlan
 vconfig add $INTERNAL_IF $VLAN
-ifconfig $INTERNAL_IF 0/0 up 2>/dev/null
 ifconfig $INTERNAL_IF.$VLAN $APACHE_VLAN_IP up
-route add default gw `grep ^VLAN_IP= vm1.config | sed 's/^VLAN_IP=\(.*\)\/.*$/\1/g'`
 
